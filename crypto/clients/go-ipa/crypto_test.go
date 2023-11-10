09868650fe01aca3586ca86a820c06fcaa2948a6
Ignacio Hagopian <jsign.uy@gmail.com>
2023-11-10 09:10:09 -0300

    crypto/go-ipa: add 002 test runner
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- crypto/clients/go-ipa/crypto_test.go
+++ crypto/clients/go-ipa/crypto_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/crate-crypto/go-ipa/bandersnatch/fr"
+	"github.com/crate-crypto/go-ipa/banderwagon"
 	"github.com/crate-crypto/go-ipa/ipa"
 	"github.com/stretchr/testify/require"
 )
@@ -36,7 +37,33 @@ func Test001(t *testing.T) {
 	comm := config.Commit(scalars).Bytes()
 	gotComm := hex.EncodeToString(comm[:])
 
-	require.Equal(t, data.TestData.SerializedCommitment, gotComm)
+	require.Equal(t, data.TestData.SerializedCommitment[2:], gotComm)
+}
+
+func Test002(t *testing.T) {
+	t.Parallel()
+
+	type testType = struct {
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		TestData    struct {
+			SerializedPoint string `json:"serializedPoint"`
+			FieldElement    string `json:"fieldElement"`
+		} `json:"testData"`
+	}
+	var data testType
+	readTestFile(t, "../../002_map_to_field_element.json", &data)
+
+	var point banderwagon.Element
+	pointBytes, err := hex.DecodeString(data.TestData.SerializedPoint[2:])
+	require.NoError(t, err)
+	err = point.SetBytes(pointBytes)
+	require.NoError(t, err)
+
+	var got fr.Element
+	point.MapToScalarField(&got)
+
+	require.Equal(t, data.TestData.FieldElement, got.String())
 }
 
 func readTestFile(t *testing.T, testFilePath string, out interface{}) {
