19048ceb73923b03f99df377b749d31adcb8d60a
Ignacio Hagopian <jsign.uy@gmail.com>
2023-12-01 09:18:14 -0300

    crypto/geth: add runner for 004
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- crypto/clients/go-ipa/crypto_test.go
+++ crypto/clients/go-ipa/crypto_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"io"
 	"os"
 	"testing"
@@ -83,7 +82,6 @@ func Test003(t *testing.T) {
 	var data testType
 	readTestFile(t, "../../003_serialize_lexicographically_highest.json", &data)
 
-	fmt.Printf("aslkdjasda: %s\n", data.TestData.SerializeXCoordinate[2:])
 	xCoordBytes, err := hex.DecodeString(data.TestData.SerializeXCoordinate[2:])
 	require.NoError(t, err)
 	yCoordBytes, err := hex.DecodeString(data.TestData.SerializeYCoordinate[2:])
@@ -91,6 +89,39 @@ func Test003(t *testing.T) {
 	var y fp.Element
 	y.SetBytes(yCoordBytes)
 	require.True(t, y.LexicographicallyLargest()) // Shouldn't be needed -- double-check test assumption.
+
+	var point banderwagon.Element
+	err = point.SetBytesUncompressed(append(xCoordBytes, yCoordBytes...), false) // Use false just to be sure.
+	require.NoError(t, err)
+
+	gotSerializedPoint := point.Bytes()
+	expSerializedPoint, err := hex.DecodeString(data.TestData.ExpectedSerializedPoint[2:])
+	require.NoError(t, err)
+	require.Equal(t, gotSerializedPoint[:], expSerializedPoint)
+}
+
+func Test004(t *testing.T) {
+	t.Parallel()
+
+	type testType = struct {
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		TestData    struct {
+			SerializeXCoordinate    string `json:"serializedXCoordinate"`
+			SerializeYCoordinate    string `json:"serializedYCoordinate"`
+			ExpectedSerializedPoint string `json:"expectedSerializedPoint"`
+		} `json:"testData"`
+	}
+	var data testType
+	readTestFile(t, "../../004_serialize_lexicographically_lowest.json", &data)
+
+	xCoordBytes, err := hex.DecodeString(data.TestData.SerializeXCoordinate[2:])
+	require.NoError(t, err)
+	yCoordBytes, err := hex.DecodeString(data.TestData.SerializeYCoordinate[2:])
+	require.NoError(t, err)
+	var y fp.Element
+	y.SetBytes(yCoordBytes)
+	require.False(t, y.LexicographicallyLargest()) // Double-check test assumption.
 
 	var point banderwagon.Element
 	err = point.SetBytesUncompressed(append(xCoordBytes, yCoordBytes...), false) // Use false just to be sure.
